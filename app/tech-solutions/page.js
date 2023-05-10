9616aa22920d95b04fb6452b2bc95fdf860230c3
Mohammed Safvan <mohammedsafvannk@gmail.com>
2023-05-10 14:02:15 +0530

    init commit

---
+++ app/tech-solutions/page.js
@@ -0,0 +1,14 @@
+import Hero from "@/components/Hero";
+import WeDoCard from "@/components/WeDoCard";
+import { whatWeDo } from "@/helpers/data";
+
+export default function Home() {
+  return (
+    <>
+      {/* Hero */}
+      <Hero />
+      {/*  End of hero */}
+
+    </>
+  );
+}
---
+++ helpers/data.js
@@ -0,0 +1,51 @@
+export const whatWeDo = [
+  {
+    name: "Export-Import",
+    path: "export-import",
+    imgUrl:
+      "https://images.unsplash.com/photo-1601897690942-bcacbad33e55?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitiaolestiae quas vel sint commodi repudiandae consequuntur voluptatum laborumnumquam blanditiis harum quisquam ",
+  },
+  {
+    name: "Tech Solutions",
+    path: "tech-solutions",
+    imgUrl:
+      "https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitiaolestiae quas vel sint commodi repudiandae consequuntur voluptatum laborumnumquam blanditiis harum quisquam ",
+  },
+  {
+    name: "Production",
+    path: "production",
+    imgUrl:
+      "https://images.unsplash.com/photo-1513828646384-e4d8ec30d2bb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitiaolestiae quas vel sint commodi repudiandae consequuntur voluptatum laborumnumquam blanditiis harum quisquam ",
+  },
+  {
+    name: "Tours & Travels",
+    path: "tours-travels",
+    imgUrl:
+      "https://images.unsplash.com/photo-1552733407-5d5c46c3bb3b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1980&q=80",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitiaolestiae quas vel sint commodi repudiandae consequuntur voluptatum laborumnumquam blanditiis harum quisquam ",
+  },
+  {
+    name: "Farming",
+    path: "farming",
+    imgUrl:
+      "https://images.unsplash.com/photo-1625246333195-78d9c38ad449?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitiaolestiae quas vel sint commodi repudiandae consequuntur voluptatum laborumnumquam blanditiis harum quisquam ",
+  },
+  {
+    name: "Food & Beverages",
+    path: "food-beverages",
+    imgUrl:
+      "https://images.unsplash.com/photo-1615522894493-2f3e3fda615c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitiaolestiae quas vel sint commodi repudiandae consequuntur voluptatum laborumnumquam blanditiis harum quisquam ",
+  },
+  {
+    name: "Charity",
+    path: "charity",
+    imgUrl:
+      "https://images.unsplash.com/photo-1643321613219-6d50e1372c0a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1887&q=80",
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime mollitiaolestiae quas vel sint commodi repudiandae consequuntur voluptatum laborumnumquam blanditiis harum quisquam ",
+  },
+];
